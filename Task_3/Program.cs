7b02b7702313ea91c8c9724eeb6588e5a2fd6104
Alexey Nechaeff <nechaeff91@gmail.com>
2022-07-23 18:51:48 +0500

    Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] getNewArray(int lenghtArr)
+{
+int[] array = new int[lenghtArr];
+
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(int.MinValue, int.MaxValue);
+}
+
+
+return array;
+}
+
+
+foreach (int item in getNewArray(8))
+{
+Console.Write(item + " ");
+}
