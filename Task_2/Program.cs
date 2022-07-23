2c7749031a537534eb23931f8767f364ba50f6b6
Alexey Nechaeff <nechaeff91@gmail.com>
2022-07-23 18:24:42 +0500

    Task 2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int getDeg(int a)
+{
+    int sum = 0;
+string str = a.ToString();
+int[] array = new int[str.Length];
+for( int i=0; i< str.Length; i++)
+{
+     array[i] = int.Parse(str[i].ToString());
+    sum = sum + array[i];
+}
+return sum;
+}
+
+Console.WriteLine("Введите число = ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine(getDeg(num));
+
+
